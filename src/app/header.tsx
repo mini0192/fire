322157338b70c86ef14b6b37a32e95c7a1399bef
mini0192 <parkgw9071@gmail.com>
2025-03-05 14:05:13 +0900

    재밋는거 없나..?

---
+++ src/app/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <>
+            <div className="flex justify-center gap-4">
+            <h3 className="inline">기능1</h3>
+            <h3 className="inline">기능2</h3>
+            </div>
+            <p>----------------------------------------------------------_</p>
+        </>
+      );
+      
+}
